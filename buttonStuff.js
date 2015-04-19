b25f50c936304adecd0b449c5fc1234cb8384ee1
Tina Howard <chh2538@rit.edu>
2015-04-19 02:03:39 -0400

    sending put requests for visit

--- buttonStuff.js
+++ buttonStuff.js
@@ -1,6 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
 	
-	console.log(chrome.tabs.getCurrent);
+	  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+    var host = url.split("/")[2];
+    var visitRequest = new XMLHttpRequest();
+    var link = "http://private-anon-bbd6718d7-meaola.apiary-mock.com/creatures/1/visit/" + host;
+    visitRequest.open("PUT", link);
+    visitRequest.send();
+    visitRequest.onreadystatechange = function() {
+	    if(this.readyState == 4 && this.status == 204){
+	    	console.log(host);
+	    	console.log("sent the url");
+	    }
+	}
+
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+
+  });
+
 	var feed = document.getElementById('feed-button');
 	var sleep = document.getElementById('sleep-button');
 	feed.addEventListener("click", feedCreature);
@@ -59,3 +96,4 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 });
+
