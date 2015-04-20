6368644c981b9f4298cb686eafcb97883b9496d1
Jesse Jurman <jrjurman@gmail.com>
2015-04-19 23:18:32 -0400

    removed file, add visitpage logic

--- visitPage.js
+++ visitPage.js
@@ -13,3 +13,33 @@ visitRequest.onreadystatechange = function() {
     console.log("send the url");
   }
 }
+
+var creatureInfo = new XMLHttpRequest();
+creatureInfo.open("GET", server + "/creatures/1", true);
+creatureInfo.send();
+
+creatureInfo.onreadystatechange = function() {
+  var statusImage = document.getElementById('image-result');
+
+  if(this.readyState == 4 && this.status == 200){
+
+    var response = JSON.parse(this.response);
+    console.log(response.status);
+    console.log(chrome);
+
+    if(response.status == "skills"){
+      chrome.browserAction.setIcon({path:"gifs/skill/0003.png"});
+    }else if(response.status == "defense"){
+      chrome.browserAction.setIcon({path:"gifs/def/0000.png"});
+    }else if(response.status == "intelligence"){
+      chrome.browserAction.setIcon({path:"gifs/int/0001.png"});
+    }else if(response.status == "tired" || response.status == "sleeping"){
+      chrome.browserAction.setIcon({path:"gifs/sleep/0000.png"});
+    }else if(response.status == "hungry" || response.status == "eating"){
+      chrome.browserAction.setIcon({path:"gifs/eat/0002.png"});
+    }else{//idle
+      chrome.browserAction.setIcon({path:"gifs/neutral/0012.png"});
+    }
+
+  }
+}
