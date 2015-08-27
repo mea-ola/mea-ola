7238633b1984ba2e72b14afe484509aa874465b6
Ethan H. Jurman <ehj2229@rit.edu>
2015-08-27 14:21:53 -0400

    Update 2015-08-27T18:21:49.124Z

--- src/js/main.jsx
+++
@@ -1,8 +0,0 @@
-// main.jsx
-// main javascript application, which renders the Mea-Ola Garden
-
-var React = require('react');
-var GardenWindow = require('./components/gardenWindow');
-//var flux = require('./flux');
-
-React.render(<GardenWindow />, document.getElementById("main"));
